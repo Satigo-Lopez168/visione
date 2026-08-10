fb86ab0313ee060624fee2aac271394b7864a466
Angélica R  Lopez D <angelicalopezd@Mac.lan>
2026-08-10 13:04:36 -0500

    add animation

--- visione/app/page.tsx
+++ visione/app/page.tsx
@@ -1,10 +1,15 @@
 "use client"
-import Image from "next/image";
-import { useState } from "react";
+import { gsap } from "gsap";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [clickCounter, setClickCounter] = useState(0)
   const [startAnimation, setStartAnimation] = useState(false)
+
+  const clickTextElement= gsap.to("#click-element", { rotation: 360, duration: 1, ease: "elastic" })
+  useEffect(()=>{
+    clickTextElement.play()
+  },[clickCounter])
   return (
     <div className="bg-purple-500 flex flex-col">
 
@@ -12,7 +17,7 @@ export default function Home() {
         setClickCounter(clickCounter + 1)
         setStartAnimation(true)
       }}>click</button>
-      <div className={`flex justify-content-center text-white ${startAnimation && 'animation-ping'}`}>haz hecho {clickCounter} clicks </div>
+      <div id="click-element" className={`flex justify-center text-white ${startAnimation && 'animation-ping'}`}>haz hecho {clickCounter} clicks </div>
     </div>
 
   );
