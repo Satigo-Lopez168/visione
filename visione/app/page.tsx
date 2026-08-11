555912b83265a67ff32092bc2b6f17d311c97b8f
Angélica R  Lopez D <angelicalopezd@Mac.lan>
2026-08-11 16:42:12 -0500

    improve animaton

--- visione/app/page.tsx
+++ visione/app/page.tsx
@@ -1,23 +1,28 @@
 "use client"
 import { gsap } from "gsap";
-import { useEffect, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import { useEffect, useState, useRef} from "react";
 
 export default function Home() {
   const [clickCounter, setClickCounter] = useState(0)
   const [startAnimation, setStartAnimation] = useState(false)
-
-  const clickTextElement= gsap.to("#click-element", { rotation: 360, duration: 1, ease: "elastic" })
-  useEffect(()=>{
-    clickTextElement.play()
-  },[clickCounter])
+  const container=useRef(null)
+  const clickTextElement=useRef(null)
+  gsap.registerPlugin(useGSAP);
+  useGSAP(()=>{
+   gsap.to(clickTextElement.current, { scaleX:2, duration: 0.050, ease:"elastic",yoyo:true,repeat:1 }) 
+  },{
+    scope:container,
+    dependencies:[clickCounter]
+  })
   return (
-    <div className="bg-purple-500 flex flex-col">
+    <div ref={container} className="bg-purple-500 flex flex-col gap-20" >
 
-      <button onClick={() => {
+      <button className="w-xl h-1/3 border-solid border-2 self-center" onClick={() => {
         setClickCounter(clickCounter + 1)
         setStartAnimation(true)
       }}>click</button>
-      <div id="click-element" className={`flex justify-center text-white ${startAnimation && 'animation-ping'}`}>haz hecho {clickCounter} clicks </div>
+      <div ref={clickTextElement} className={`flex justify-center text-white ${startAnimation && 'animation-ping'}`}>haz hecho <div className="px-1"> {` ${clickCounter} `} </div> clicks </div>
     </div>
 
   );
